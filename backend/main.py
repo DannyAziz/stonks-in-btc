91c7342d02bb3e73ba9f88ce51156ed568b46778
Danny Aziz <daanyaal997@gmail.com>
2021-01-11 08:41:01 +0000

    Forgot a comma

--- backend/main.py
+++ backend/main.py
@@ -15,7 +15,7 @@ app = FastAPI()
 origins = [
     "http://localhost:8000",
     "http://localhost:3000",
-    "https://stonksinbtc.xyz"
+    "https://stonksinbtc.xyz",
     "https://api.stonksinbtc.xyz"
 ]
 
